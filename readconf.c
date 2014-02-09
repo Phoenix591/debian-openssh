d0cbcbf53d5f0d4457b47a09af06aac1f483e712
Colin Watson <cjwatson@ubuntu.com>
2014-02-09 16:09:50 +0000

    Accept obsolete ssh-vulnkey configuration options
    These options were used as part of Debian's response to CVE-2008-0166.
    Nearly six years later, we no longer need to continue carrying the bulk
    of that patch, but we do need to avoid failing when the associated
    configuration options are still present.
    
    Last-Update: 2014-02-09
    
    Patch-Name: ssh-vulnkey-compat.patch

--- readconf.c
+++ readconf.c
@@ -199,6 +199,7 @@ static struct {
 	{ "fallbacktorsh", oDeprecated },
 	{ "globalknownhostsfile2", oDeprecated },
 	{ "rhostsauthentication", oDeprecated },
+	{ "useblacklistedkeys", oDeprecated },
 	{ "userknownhostsfile2", oDeprecated },
 	{ "useroaming", oDeprecated },
 	{ "usersh", oDeprecated },
