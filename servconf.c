1dce8eb1d74c508042e2d7ee2a4405a6870e6bf5
Kees Cook <kees@debian.org>
2014-02-09 16:10:06 +0000

    Add DebianBanner server configuration option
    Setting this to "no" causes sshd to omit the Debian revision from its
    initial protocol handshake, for those scared by package-versioning.patch.
    
    Bug-Debian: http://bugs.debian.org/562048
    Forwarded: not-needed
    Last-Update: 2024-09-22
    
    Patch-Name: debian-banner.patch

--- servconf.c
+++ servconf.c
@@ -219,6 +219,7 @@ initialize_server_options(ServerOptions *options)
 	options->unused_connection_timeout = -1;
 	options->sshd_session_path = NULL;
 	options->refuse_connection = -1;
+	options->debian_banner = -1;
 }
 
 /* Returns 1 if a string option is unset or set to "none" or 0 otherwise. */
@@ -507,6 +508,8 @@ fill_default_server_options(ServerOptions *options)
 		options->sshd_session_path = xstrdup(_PATH_SSHD_SESSION);
 	if (options->refuse_connection == -1)
 		options->refuse_connection = 0;
+	if (options->debian_banner == -1)
+		options->debian_banner = 1;
 
 	assemble_algorithms(options);
 
@@ -591,6 +594,7 @@ typedef enum {
 	sExposeAuthInfo, sRDomain, sPubkeyAuthOptions, sSecurityKeyProvider,
 	sRequiredRSASize, sChannelTimeout, sUnusedConnectionTimeout,
 	sSshdSessionPath, sRefuseConnection,
+	sDebianBanner,
 	sDeprecated, sIgnore, sUnsupported
 } ServerOpCodes;
 
@@ -770,6 +774,7 @@ static struct {
 	{ "unusedconnectiontimeout", sUnusedConnectionTimeout, SSHCFG_ALL },
 	{ "sshdsessionpath", sSshdSessionPath, SSHCFG_GLOBAL },
 	{ "refuseconnection", sRefuseConnection, SSHCFG_ALL },
+	{ "debianbanner", sDebianBanner, SSHCFG_GLOBAL },
 	{ NULL, sBadOption, 0 }
 };
 
@@ -2754,6 +2759,10 @@ process_server_config_line_depth(ServerOptions *options, char *line,
 		multistate_ptr = multistate_flag;
 		goto parse_multistate;
 
+	case sDebianBanner:
+		intptr = &options->debian_banner;
+		goto parse_flag;
+
 	case sDeprecated:
 	case sIgnore:
 	case sUnsupported:
@@ -3307,6 +3316,7 @@ dump_config(ServerOptions *o)
 	dump_cfg_fmtint(sFingerprintHash, o->fingerprint_hash);
 	dump_cfg_fmtint(sExposeAuthInfo, o->expose_userauth_info);
 	dump_cfg_fmtint(sRefuseConnection, o->refuse_connection);
+	dump_cfg_fmtint(sDebianBanner, o->debian_banner);
 
 	/* string arguments */
 	dump_cfg_string(sPidFile, o->pid_file);
